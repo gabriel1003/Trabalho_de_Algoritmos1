9cc868e6a68697033785efae8663f1b6e7108698
gabriel1003 <merconigabriel22@gmail.com>
2024-06-28 22:48:33 -0300

    Corrigir bug de reinicialisacao

--- Jogo_da_Velha.cpp
+++ Jogo_da_Velha.cpp
@@ -2,34 +2,25 @@
 #include <ctime>
 #include <cstdlib> // Para usar a função rand()
 using namespace std;
+
 const int LINHAS = 6;
 const int COLUNAS = 6;
 
-void imprimir_matriz(int m[LINHAS][COLUNAS], int qtdLinhas, int qtdColunas)
-{
-    for (int i = 0; i < qtdColunas; i++)
-    {
+// Função para imprimir a matriz do jogo
+void imprimir_matriz(int m[LINHAS][COLUNAS], int qtdLinhas, int qtdColunas) {
+    for (int i = 0; i < qtdColunas; i++) {
         cout << '\t' << i;
     }
     cout << endl;
-    for (int l = 0; l < qtdLinhas; l++)
-    {
+    for (int l = 0; l < qtdLinhas; l++) {
         cout << l;
-        for (int c = 0; c < qtdColunas; c++)
-        {
-            if (m[l][c] == 100)
-            {
+        for (int c = 0; c < qtdColunas; c++) {
+            if (m[l][c] == 100) {
                 cout << "\t#";
-            }
-            else
-            {
-                if (m[l][c] == 200)
-                {
-                    cout << "\tx";
-                }
-                else {
-                    cout << '\t' << m[l][c];
-                }
+            } else if (m[l][c] == 200) {
+                cout << "\tx";
+            } else {
+                cout << '\t' << m[l][c];
             }
         }
         cout << endl;
@@ -77,7 +68,7 @@ int main() {
         int l = 0, c = 0;
 
         switch (opcao) {
-        case 1:
+        case 1: // Jogar em dupla
             cout << "\tDigite 1 para ser o jogador (#) e 2 para ser o (x)\n"
                  << "\tqual jogador você quer ser?";
             cin >> opcao;
@@ -90,8 +81,10 @@ int main() {
                      << "\tVocê sera representado no jogo pelo número 200 e seu amigo sera o número 100" << endl;
             } else {
                 cout << "\tDigite uma opção válida." << endl;
+                break; // Sai do case 1 se a opção for inválida
             }
 
+            // Escolher o tamanho do tabuleiro
             cout << "\tAgora escolha o tabuleiro.\n"
                  << "\tDigite um para tabuleiro simples com (3) linhas e (3) colunas;\n"
                  << "\tDigite 2 para tabuleiro intermediario com 6 linhas e colunas;\n"
@@ -114,10 +107,94 @@ int main() {
                 break;
             default:
                 cout << "\nDigite uma opção válida." << endl;
-                break;
+                break; // Sai do case 1 se a opção for inválida
             }
-            break;
-        case 2:
+
+            // Escolher jogador inicial aleatoriamente
+            srand(time(0));
+            jogador_atual = (rand() % 2) + 1;
+            cout << "O jogador " << jogador_atual << " começa!" << endl;
+
+            // Loop principal do jogo em dupla
+            while (!jogo_terminou) {
+                imprimir_matriz(m, qtd_linhas, qtd_colunas);
+
+                // Jogada do jogador atual
+                if (jogador_atual == 1) {
+                    cout << "Jogador #, digite a linha e coluna que você deseja (separadas por espaço): ";
+                    cin >> l >> c;
+
+                    // Validar a posição
+                    while (l < 0 || l >= qtd_linhas || c < 0 || c >= qtd_colunas || m[l][c] != 0) {
+                        cout << "Posição inválida. Tente novamente: ";
+                        cin >> l >> c;
+                    }
+
+                    m[l][c] = jogador1;
+                } else {
+                    cout << "Jogador x, digite a linha e coluna que você deseja (separadas por espaço): ";
+                    cin >> l >> c;
+
+                    // Validar a posição
+                    while (l < 0 || l >= qtd_linhas || c < 0 || c >= qtd_colunas || m[l][c] != 0) {
+                        cout << "Posição inválida. Tente novamente: ";
+                        cin >> l >> c;
+                    }
+
+                    m[l][c] = jogador2;
+                }
+
+                // Verificar se o jogo terminou (linha, coluna, diagonais)
+                for (int i = 0; i < qtd_linhas; i++) {
+                    // Verificar linhas
+                    if (m[i][0] == m[i][1] && m[i][1] == m[i][2] && m[i][0] != 0) {
+                        jogo_terminou = true;
+                        if (m[i][0] == jogador1) {
+                            cout << "Jogador # venceu!" << endl;
+                        } else {
+                            cout << "Jogador x venceu!" << endl;
+                        }
+                    }
+                    // Verificar colunas
+                    if (m[0][i] == m[1][i] && m[1][i] == m[2][i] && m[0][i] != 0) {
+                        jogo_terminou = true;
+                        if (m[0][i] == jogador1) {
+                            cout << "Jogador # venceu!" << endl;
+                        } else {
+                            cout << "Jogador x venceu!" << endl;
+                        }
+                    }
+                }
+                // Verificar diagonais
+                if ((m[0][0] == m[1][1] && m[1][1] == m[2][2] && m[0][0] != 0) ||
+                    (m[0][2] == m[1][1] && m[1][1] == m[2][0] && m[0][2] != 0)) {
+                    jogo_terminou = true;
+                    if (m[1][1] == jogador1) {
+                        cout << "Jogador # venceu!" << endl;
+                    } else {
+                        cout << "Jogador x venceu!" << endl;
+                    }
+                }
+
+                // Verificar empate
+                if (!jogo_terminou) {
+                    int contador = 0;
+                    for (int i = 0; i < qtd_linhas; i++) {
+                        for (int j = 0; j < qtd_colunas; j++) {
+                            if (m[i][j] != 0) {
+                                contador++;
+                            }
+                        }
+                    }
+                    if (contador == qtd_linhas * qtd_colunas) {
+                        jogo_terminou = true;
+                        cout << "Empate!" << endl;
+                    }
+                }
+
+            }
+            break; // Sai do case 1 após o jogo terminar
+        case 2: // Jogar contra o computador
             cout << "\tDigite 1 para ser o jogador (#) e 2 para ser o (x)\n"
                  << "\tqual jogador você quer ser?";
             cin >> opcao;
@@ -131,8 +208,10 @@ int main() {
                      << "\tVocê sera representado no jogo pelo número 200 e o computador sera o número 100" << endl;
             } else {
                 cout << "\tDigite uma opção válida." << endl;
+                break; // Sai do case 2 se a opção for inválida
             }
 
+            // Escolher o tamanho do tabuleiro
             cout << "\tAgora escolha o tabuleiro.\n"
                  << "\tDigite um para tabuleiro simples com (3) linhas e (3) colunas;\n"
                  << "\tDigite 2 para tabuleiro intermediario com 6 linhas e colunas;\n"
@@ -155,32 +234,19 @@ int main() {
                 break;
             default:
                 cout << "\nDigite uma opção válida." << endl;
-                break;
+                break; // Sai do case 2 se a opção for inválida
             }
-            break;
-        case 3:
-            cout << "\tJogo desenvolvido por gabriel Samersla merçoni para a matéria de algoritmos 1 do professor Eduardo Alves da Silva." << endl;
-            break;
-        case 0:
-            cout << "\tAté a próxima!";
-            return 0;
-        default:
-            cout << "\tDigite uma opção válida." << endl;
-            break;
-        }
 
-        // Verificar se as dimensões do tabuleiro foram definidas
-        if (qtd_linhas > 0 && qtd_colunas > 0) {
-            // Escolher o jogador inicial aleatoriamente
-            srand(time(0)); // Inicializa o gerador de números aleatórios
-            jogador_atual = (rand() % 2) + 1; // Retorna 1 ou 2
+            // Escolher jogador inicial aleatoriamente
+            srand(time(0));
+            jogador_atual = (rand() % 2) + 1;
             cout << "O jogador " << jogador_atual << " começa!" << endl;
 
-            // Loop principal do jogo
+            // Loop principal do jogo contra o computador
             while (!jogo_terminou) {
-                imprimir_matriz(m, qtd_linhas, qtd_colunas); // Exibir o tabuleiro
+                imprimir_matriz(m, qtd_linhas, qtd_colunas);
 
-                // Jogada do jogador
+                // Jogada do jogador atual
                 if (jogador_atual == 1) {
                     cout << "Jogador #, digite a linha e coluna que você deseja (separadas por espaço): ";
                     cin >> l >> c;
@@ -192,16 +258,12 @@ int main() {
                     }
 
                     m[l][c] = jogador1;
-                }
-                // Jogada do computador
-                else {
-                    // Lógica do computador:
+                } else {
+                    // Lógica do computador
+                    cout << "Computador jogando..." << endl;
                     if (computador_defensivo) {
-                        // Defensivo (jogador começou)
-                        // ... lógica para jogadas defensivas ...
-                        cout << "Computador jogando..." << endl;
-
-                        //  Buscar posição válida para bloquear
+                        // Lógica defensiva
+                        // Buscar posição válida para bloquear
                         for (int i = 0; i < qtd_linhas; i++) {
                             for (int j = 0; j < qtd_colunas; j++) {
                                 if (m[i][j] == 0) {
@@ -262,9 +324,7 @@ int main() {
                         }
                         m[l][c] = jogador2;
                     } else {
-                        // Ofensivo (computador começou)
-                        // ... lógica para jogadas ofensivas ...
-                        cout << "Computador jogando..." << endl;
+                        // Lógica ofensiva
                         // Escolha aleatória para o computador
                         srand(time(0));
                         l = rand() % qtd_linhas;
@@ -329,6 +389,16 @@ fim_jogada_computador: ; // Rótulo para sair do loop
                 // Mudar o jogador
                 jogador_atual = (jogador_atual == 1) ? 2 : 1;
             }
+            break; // Sai do case 2 após o jogo terminar
+        case 3:
+            cout << "\tJogo desenvolvido por gabriel Samersla merçoni para a matéria de algoritmos 1 do professor Eduardo Alves da Silva." << endl;
+            break;
+        case 0:
+            cout << "\tAté a próxima!";
+            return 0;
+        default:
+            cout << "\tDigite uma opção válida." << endl;
+            break;
         }
     } while (opcao != 0);
 
